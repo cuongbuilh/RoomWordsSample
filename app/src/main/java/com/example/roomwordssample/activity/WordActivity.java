269da1a8ae75ea9386e294ea7f4406d1f40602c4
Bui Cuong <cuongbuilh@gmail.com>
2022-04-16 17:24:28 +0700

    hide delete button

--- app/src/main/java/com/example/roomwordssample/activity/WordActivity.java
+++ app/src/main/java/com/example/roomwordssample/activity/WordActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.roomwordssample.R;
@@ -23,6 +24,7 @@ public class WordActivity extends AppCompatActivity {
 
     private EditText mEditWordView;
     private int idIntent;
+    private Button btn_delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +32,15 @@ public class WordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_word);
         mEditWordView = findViewById(R.id.edit_word);
 
+        btn_delete = findViewById(R.id.button_delete);
+        btn_delete.setVisibility(View.INVISIBLE);
+
         Intent intent = getIntent();
         String wordIntent = intent.getStringExtra("word");
         idIntent = intent.getIntExtra("id", -1);
         if (wordIntent != null) {
             mEditWordView.setText(wordIntent);
+            btn_delete.setVisibility(View.VISIBLE);
         }
 
     }
